67dae76a8e5e85cd8c606d2559ed01e2ea57a898
Christopher Lim <toffer.lim87@gmail.com>
2021-12-06 19:45:13 -0800

    remove unused files, fix readme, add parallel config

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
 # WDIO APPIUM BROWSERSTACK
-npm install --save webdriverio @wdio/cli
-npm install --save chai
+*** This is a POC to run to run Appium Wdio in Browser Stack ***
+
+### Installation of node packages
+```
+npm install --save @wdio/cli @wdio/local-runner @wdio/mocha-framework
 npm install --save browserstack-local
+```
+
+### Config Wdio Config
+```
 npx wdio config
------
-Where is your automation backend located? On my local machine
-Which framework do you want to use? mocha
-Do you want to use a compiler? No!
-Where are your test specs located? ./test/specs/**/*.js
-Do you want WebdriverIO to autogenerate some test files? Yes
-Do you want to use page objects? Yes
-Where are your page objects located? ./test/pageobjects/**/*.js
-Which reporter do you want to use? spec
-Do you want to add a service to your test setup? selenium-standalone
-What is the base url? http://localhost
-------
+```
+
--- parallel.conf.js
+++ parallel.conf.js
@@ -7,15 +7,22 @@ exports.config = {
     './test/specs/**/*.js'
   ],
   exclude: [],
-  
-  capabilities: [{
+
+  maxInstances: 10,
+  commonCapabilities: {
     project: "Wdio Appium Browserstack",
     build: 'Webdriverio Android',
-    name: 'Login Test',
-    device: 'Google Pixel 3',
-    os_version: "9.0",
+    name: 'Parallel Login Test',
     app: process.env.BROWSERSTACK_APP_ID,
     'browserstack.debug': true
+  },
+
+  capabilities: [{
+    device: 'Google Pixel 3',
+    os_version: "9.0"
+  }, {
+    device: 'Samsung Galaxy S10e',
+    os_version: "9.0"
   }],
 
   logLevel: 'info',
@@ -25,9 +32,15 @@ exports.config = {
   waitforTimeout: 10000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
+
   framework: 'mocha',
   mochaOpts: {
     ui: 'bdd',
     timeout: 20000
   }
 };
+
+// Code to support common capabilities
+exports.config.capabilities.forEach(function(caps){
+  for(var i in exports.config.commonCapabilities) caps[i] = caps[i] || exports.config.commonCapabilities[i];
+});
